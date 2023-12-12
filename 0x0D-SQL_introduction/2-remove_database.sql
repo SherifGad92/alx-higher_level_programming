d77eeb04b5202badc0175f7a4b73d9a50c0ea22f
SherifGad92 <sherif.abdelhamid13@gmail.com>
2023-12-12 12:11:01 -0800

    0x0D. SQL - Introduction

---
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -0,0 +1,3 @@
+--DELETES DATABASE
+--If the database hbtn_0c_0 doesn’t exist, script should not fail
+DROP DATABASE IF EXISTS hbtn_0c_0;
---
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -0,0 +1,3 @@
+--count records
+--count records with id = 89 in the table first_table
+SELECT COUNT(*) FROM first_table WHERE id = 89;
